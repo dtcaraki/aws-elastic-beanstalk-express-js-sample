9933c316e708acb3bc795b26f28d04881a077cf6
dtcaraki <masaraki@tohmatsu.co.jp>
2021-12-28 11:23:20 +0900

    Second Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi this is deploy thorough manual review by Pipleline lol !')
+app.get('/', (req, res) => res.send('Hi this is deploy thorough manual review by Pipleline )
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
